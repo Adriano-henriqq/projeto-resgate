b2e6af048cc6df5dccce694de01a12c005e43be0
Adriano-henriqq <112038636+Adriano-henriqq@users.noreply.github.com>
2024-03-02 13:16:52 -0300

    Update enviaEmail.js

--- enviaEmail.js
+++ enviaEmail.js
@@ -12,13 +12,11 @@ async function enviaEmail(transporter,dados, res) {
                 console.log(`Email vazio, para o nome ${nome} pulando para o proximo email`)
             }
             let dadosEmail = {
-                from: `Comissão de Seleção Permanente das Forças Armadas 10 <${process.env.EMAIL}>`,
+                from: `seu email`,
                 to: email,
-                subject: 'Aviso de Débito no Alistamento Obrigatório',
-                html: `<h1>Olá ${nome}, Você está em débito com o alistamento obrigatório</h1> 
-                <p>A Comissão de Seleção Permanente das Forças Armadas 10, subordinada à 1ª Região Militar, no âmbito do Comando Militar do Leste verificou que o senhor não compareceu a esta CS na data agendada previamente. 
-                Desta forma, é indispensável que entre em contato conosco no nosso número de WhatsApp: <a href="https://wa.me/message/FNJ3IPCGOJCIA1">Link do nosso WhatsApp</a> ou <a href="https://wa.me/message/7YSH6H5AMMLHI1">Link do nosso WhatsApp</a> para o agendamento da sua visita junto à CSPFA 10, para regularizar sua situação. A comissão fica 
-                situada na Rua Licínio Cardoso, 96 - São Francisco Xavier, Rio de Janeiro - RJ, CEP 20911-015, em frente a estação de Metrô de triagem. Caso já tenha comparecido, favor desconsiderar esta mensagem.</p>`,
+                subject: 'assunto do email',
+                html: `<h1>Olá ${nome}</h1> 
+                <p>sua mensagem.</p>`,
                 
             }
             
